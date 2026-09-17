39daf5aab4a553d36732c4e61528fd23e369c078
sondeda <sondeda@gmail.com>
2026-09-17 20:32:02 +0300

    fix il2cpp path

--- src/main.cpp
+++ src/main.cpp
@@ -43,8 +43,8 @@ template<typename T> struct Il2CppList {
 };
 
 static bool load_il2cpp() {
-    void* h = dlopen("libil2cpp.so", RTLD_NOW|RTLD_NOLOAD);
-    if (!h) h = dlopen("libil2cpp.so", RTLD_NOW);
+    void* h = dlopen("/data/app/~~_NthGXSYS_U0l4s9w1s57g==/com.catsbit.oxidesurvivalisland-bT7-xLtSAW6BuiuElJXNMw==/lib/arm64/libil2cpp.so", RTLD_NOW|RTLD_NOLOAD);
+    if (!h) h = dlopen("/data/app/~~_NthGXSYS_U0l4s9w1s57g==/com.catsbit.oxidesurvivalisland-bT7-xLtSAW6BuiuElJXNMw==/lib/arm64/libil2cpp.so", RTLD_NOW);
     if (!h) { LOGE("dlopen failed: %s", dlerror()); return false; }
 #define SYM(fn,name) fn=(decltype(fn))dlsym(h,name); if(!fn){LOGE("dlsym %s failed",name);return false;}
     SYM(il2cpp_domain_get,"il2cpp_domain_get")
