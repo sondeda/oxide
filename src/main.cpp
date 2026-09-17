8491b22d08e9f8c3534d426e32958add9ac77994
sondeda <sondeda@gmail.com>
2026-09-17 20:42:47 +0300

    fix dlopen

--- src/main.cpp
+++ src/main.cpp
@@ -43,8 +43,7 @@ template<typename T> struct Il2CppList {
 };
 
 static bool load_il2cpp() {
-    void* h = dlopen("/data/app/~~_NthGXSYS_U0l4s9w1s57g==/com.catsbit.oxidesurvivalisland-bT7-xLtSAW6BuiuElJXNMw==/lib/arm64/libil2cpp.so", RTLD_NOW|RTLD_GLOBAL);
-    if (!h) h = dlopen("/data/app/~~_NthGXSYS_U0l4s9w1s57g==/com.catsbit.oxidesurvivalisland-bT7-xLtSAW6BuiuElJXNMw==/lib/arm64/libil2cpp.so", RTLD_NOW);
+    void* h = dlopen(nullptr, RTLD_NOW);
     if (!h) { LOGE("dlopen failed: %s", dlerror()); return false; }
 #define SYM(fn,name) fn=(decltype(fn))dlsym(h,name); if(!fn){LOGE("dlsym %s failed",name);return false;}
     SYM(il2cpp_domain_get,"il2cpp_domain_get")
@@ -165,5 +164,3 @@ public:
 };
 
 REGISTER_ZYGISK_MODULE(OxideModule)
-
-extern "C" __attribute__((visibility("default"))) int zygisk_module_abi_version() { return 4; }
